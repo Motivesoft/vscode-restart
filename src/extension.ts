48264a57dd5adcdb9e010500b6d9efb413dc7b74
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-11-23 20:15:11 +0100

    Add quit commands

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Extension "vscode-restart" is now active');
 
-	context.subscriptions.push(vscode.commands.registerCommand('vscode-restart.prompt', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-restart.prompt-restart', () => {
 	    vscode.window.showWarningMessage('Are you sure you want to restart VS Code?', 'Yes', 'No')
         .then(selection => {
             if (selection === 'Yes') {
@@ -13,8 +13,21 @@ export function activate(context: vscode.ExtensionContext) {
         });
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('vscode-restart.no-prompt', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-restart.no-prompt-restart', () => {
 		restart();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-restart.prompt-quit', () => {
+	    vscode.window.showWarningMessage('Are you sure you want to quit VS Code?', 'Yes', 'No')
+        .then(selection => {
+            if (selection === 'Yes') {
+				quit();
+            }
+        });
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-restart.no-prompt-quit', () => {
+		quit();
 	}));
 }
 
@@ -23,4 +36,9 @@ export function deactivate() {}
 function restart() {
 	vscode.window.showInformationMessage('Code is restarting');
 	vscode.commands.executeCommand("workbench.action.reloadWindow");
+}
+
+function quit() {
+	vscode.window.showInformationMessage('Code is quitting');
+	vscode.commands.executeCommand("workbench.action.quit");
 }
